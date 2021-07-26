07d374b2eca1ca24aacd6baabbf8f2e8250d7e53
attriaayush <aayush.attri@red-badger.com>
2021-07-26 17:42:03 +0100

    adding further setup for the gke cluster

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -24,3 +24,42 @@ const cluster = new gcp.container.Cluster(CLUSTER_NAME, {
 });
 
 export const clusterName = cluster.name;
+
+// Manufacture a GKE-style kubeconfig. Note that this is slightly "different"
+// because of the way GKE requires gcloud to be in the picture for cluster
+// authentication (rather than using the client cert/key directly).
+export const kubeconfig = pulumi
+  .all([cluster.name, cluster.endpoint, cluster.masterAuth])
+  .apply(([name, endpoint, masterAuth]) => {
+    const context = `${gcp.config.project}_${gcp.config.zone}_${name}`;
+    return `apiVersion: v1
+clusters:
+- cluster:
+    certificate-authority-data: ${masterAuth.clusterCaCertificate}
+    server: https://${endpoint}
+  name: ${context}
+contexts:
+- context:
+    cluster: ${context}
+    user: ${context}
+  name: ${context}
+current-context: ${context}
+kind: Config
+preferences: {}
+users:
+- name: ${context}
+  user:
+    auth-provider:
+      config:
+        cmd-args: config config-helper --format=json
+        cmd-path: gcloud
+        expiry-key: '{.credential.token_expiry}'
+        token-key: '{.credential.access_token}'
+      name: gcp
+`;
+  });
+
+// Create a Kubernetes provider instance that uses our cluster from above.
+const clusterProvider = new k8s.Provider(CLUSTER_NAME, {
+  kubeconfig,
+});
