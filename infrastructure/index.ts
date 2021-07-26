00da55fd66605abe31987715c9ef8ed8c2ff7376
attriaayush <aayush.attri@red-badger.com>
2021-07-26 16:52:44 +0100

    adding config for basic cluster

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,1 +1,26 @@
+import * as k8s from "@pulumi/kubernetes";
 import * as pulumi from "@pulumi/pulumi";
+import * as gcp from "@pulumi/gcp";
+
+const CLUSTER_NAME = "wasmcloud";
+
+const engineVersion = gcp.container
+  .getEngineVersions()
+  .then((v) => v.latestMasterVersion);
+
+const cluster = new gcp.container.Cluster(CLUSTER_NAME, {
+  initialNodeCount: 2,
+  minMasterVersion: engineVersion,
+  nodeVersion: engineVersion,
+  nodeConfig: {
+    machineType: "n1-standard-1",
+    oauthScopes: [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ],
+  },
+});
+
+export const clusterName = cluster.name;
