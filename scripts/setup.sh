aa7b06f03a19d92ef8ed39b09d33fc741b2d2b34
David Laban <david.laban@red-badger.com>
2021-08-02 15:02:03 +0100

    enable istio; fix some bugs

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,34 +4,38 @@ cd "$(dirname "$0")"/..
 
 # TODO: split this out into a Makefile?
 
-function wait_for_kubernetes() {
-    kubectl wait --all-namespaces --for=condition=available --timeout=60s --all deployments
+function wait_for_deployments() {
+    kubectl wait --for=condition=available --timeout=60s --all-namespaces --all deployments
+}
+
+function wait_for_pods() {
+    until kubectl wait --for=condition=ready --timeout=480s pod "$@"; do
+        echo "Waiting for $@"
+        sleep 10
+    done
 }
 
 gcloud container clusters get-credentials wasmcloud --zone europe-west2
 
-# # It's sad that this will not get a pinned version of istio, but it's the easiest way to get going
-# command -v istioctl || curl -sL https://istio.io/downloadIstioctl | sh -
-# export PATH=$PATH:$HOME/.istioctl/bin
-# istioctl operator init
+# It's sad that this will not get a pinned version of istio, but it's the easiest way to get going
+command -v istioctl || curl -sL https://istio.io/downloadIstioctl | sh -
+export PATH=$PATH:$HOME/.istioctl/bin
+istioctl operator init
 
-# until kubectl wait --for=condition=ready pod -l name=istio-operator -n istio-operator --timeout=480s; do
-#     echo "Waiting for istio operator"
-#     sleep 10
-# done
+wait_for_pods -n istio-operator -l name=istio-operator
 
-# kubectl apply -k kubernetes/00-istio/
-# wait_for_kubernetes
+kubectl apply -k kubernetes/00-istio/
+wait_for_deployments
 
 kubectl apply -k kubernetes/01-nats-prereqs
 kubectl apply -k kubernetes/10-nats-operator
 kubectl apply -k kubernetes/20-nats-cluster
-kubectl wait --for=condition=ready -n nats-cluster pod/nats-cluster-1 --timeout=60s
-wait_for_kubernetes
+wait_for_pods -n nats-cluster -l app=nats
+wait_for_deployments
 
 # Deploy application specific resources
 kubectl apply -k kubernetes/50-todo-backend
-wait_for_kubernetes
+wait_for_deployments
 
 # Waiting for actors and capabilities to be running, removing this sleep can cause panic
 sleep 15
