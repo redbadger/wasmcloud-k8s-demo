91c41942e2ac3cc488c389315acf56ec71a77791
attriaayush <aayush.attri@red-badger.com>
2021-08-03 11:30:33 +0100

    adding basic ingress

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -5,6 +5,7 @@ import * as gcp from "@pulumi/gcp";
 const CLUSTER_NAME = "wasmcloud";
 
 export const ipAddress = new gcp.compute.Address("wasmcloud-k8s-demo", {
+  name: "wasmcloud-k8s-demo",
   region: "europe-west2",
 });
 // TODO: pull in the rest of https://cloud.google.com/kubernetes-engine/docs/how-to/managed-certs if possible
