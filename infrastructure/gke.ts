78300e0d47fd1aabbbc7d6223692a84cb9176252
attriaayush <aayush.attri@red-badger.com>
2021-08-03 15:38:17 +0100

    separating out the gke into a separate file

---
+++ infrastructure/gke.ts
@@ -0,0 +1,76 @@
+import * as k8s from "@pulumi/kubernetes";
+import * as pulumi from "@pulumi/pulumi";
+import * as gcp from "@pulumi/gcp";
+
+export class GkeCluster extends pulumi.ComponentResource {
+  public clusterName: pulumi.Output<string>;
+  public provider: k8s.Provider;
+
+  constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
+    super("wasmcloud:GkeCluster", name, {}, opts);
+    const engineVersion = gcp.container
+      .getEngineVersions()
+      .then((v) => v.latestMasterVersion);
+
+    const cluster = new gcp.container.Cluster(name, {
+      name: name,
+      initialNodeCount: 2,
+      minMasterVersion: engineVersion,
+      nodeVersion: engineVersion,
+      // addonsConfig: {
+      //   configConnectorConfig: {
+      //     enabled: true,
+      //   },
+      // },
+      nodeConfig: {
+        machineType: "n1-standard-1",
+        oauthScopes: [
+          "https://www.googleapis.com/auth/compute",
+          "https://www.googleapis.com/auth/devstorage.read_only",
+          "https://www.googleapis.com/auth/logging.write",
+          "https://www.googleapis.com/auth/monitoring",
+        ],
+      },
+    });
+
+    this.clusterName = cluster.name;
+
+    // Manufacture a GKE-style kubeconfig. Note that this is slightly "different"
+    // because of the way GKE requires gcloud to be in the picture for cluster
+    // authentication (rather than using the client cert/key directly).
+    const kubeconfig = pulumi
+      .all([cluster.name, cluster.endpoint, cluster.masterAuth])
+      .apply(([name, endpoint, masterAuth]) => {
+        const context = `${gcp.config.project}_${gcp.config.zone}_${name}`;
+        return `apiVersion: v1
+clusters:
+- cluster:
+  certificate-authority-data: ${masterAuth.clusterCaCertificate}
+  server: https://${endpoint}
+name: ${context}
+contexts:
+- context:
+  cluster: ${context}
+  user: ${context}
+name: ${context}
+current-context: ${context}
+kind: Config
+preferences: {}
+users:
+- name: ${context}
+user:
+  auth-provider:
+    config:
+      cmd-args: config config-helper --format=json
+      cmd-path: gcloud
+      expiry-key: '{.credential.token_expiry}'
+      token-key: '{.credential.access_token}'
+    name: gcp
+`;
+      });
+
+    this.provider = new k8s.Provider(name, {
+      kubeconfig,
+    });
+  }
+}
