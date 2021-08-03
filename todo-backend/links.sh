80db4e6218a04fcf1be5514cd03cc2c8cbb7b3e5
David Laban <david.laban@red-badger.com>
2021-08-03 17:50:28 +0100

    check nats connectivity in links.sh

--- todo-backend/links.sh
+++ todo-backend/links.sh
@@ -1,4 +1,15 @@
-#!/bin/bash -xeu
+#!/bin/bash
+
+set -euo pipefail
+
+# wash ctl returns success even if it can't connect to anything, so we guard against it up here.
+until nats pub devnull somenoise; do
+    echo
+    echo "Could not connect to nats cluster. Please run the following in another terminal:"
+    echo "kubectl port-forward -n nats-cluster nats-cluster-1 4222:4222"
+    echo
+    sleep 10
+done
 
 ACTOR_KEY=MD3MZVXGR36UBZOH2R5ES4KY57ZVMDSLTFGKDTIY2DM6EODVD5HTIVOI
 PROVIDER_HTTP=VAG3QITQQ2ODAOWB5TTQSDJ53XK3SHBEIFNK4AYJ5RKAX2UNSCAPHA5M
