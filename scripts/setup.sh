0d69719054c0e4b60192d57e603812c7e7f8bf8d
David Laban <david.laban@red-badger.com>
2021-08-05 12:05:47 +0100

    use cluster-specific kustomizations; check with curl

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -15,11 +15,13 @@ function wait_for_pods() {
     done
 }
 
-if [ "${1:-x}" == "eks" ]; then
+CLUSTER="${1:-x}"
+
+if [ "$CLUSTER" == "eks" ]; then
     aws eks --region eu-west-2 update-kubeconfig --name wasmcloud
     # apply nginx controller
     kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.48.1/deploy/static/provider/aws/deploy.yaml
-elif [ "${1:-x}" == "gke" ]; then
+elif [ "$CLUSTER" == "gke" ]; then
     gcloud container clusters get-credentials wasmcloud --zone europe-west2
     # apply nginx controller
     kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.48.1/deploy/static/provider/cloud/deploy.yaml
@@ -41,7 +43,7 @@ wait_for_pods -n nats -l app.kubernetes.io/name=nats
 wait_for_deployments
 
 # Deploy application specific resources
-kubectl apply -k kubernetes/50-todo-backend
+kubectl apply -k "kubernetes/50-todo-backend/$CLUSTER"
 wait_for_deployments
 
 # Waiting for actors and capabilities to be running, removing this sleep can cause panic
@@ -50,3 +52,16 @@ sleep 15
 # Port forward to the nats cluster
 # TODO: work out how to make `concurrently` set a successful exit code.
 pnpx -y concurrently --kill-others "kubectl port-forward -n nats service/nats 4222:4222" "todo-backend/links.sh" || true
+
+while true; do
+    if [ "$CLUSTER" == "eks" ]; then
+        hostname=$(kubectl -n todo-backend get ingress ingress -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
+    else
+        hostname=$(kubectl -n todo-backend get ingress ingress -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+    fi
+
+    curl --fail "$hostname/api" | jq && break
+
+    echo "waiting for ingress to work"
+    sleep 10
+done
