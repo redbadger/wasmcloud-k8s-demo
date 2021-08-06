5ba17e5693de967a5c7121337eaa1b3f7e810b53
attriaayush <aayush.attri@red-badger.com>
2021-08-06 16:52:23 +0100

    exporting the outputs from resource creation

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -3,5 +3,5 @@ import * as eks from "./eks";
 
 const CLUSTER_NAME = "wasmcloud";
 
-const gkeCluster = new gke.GkeCluster(CLUSTER_NAME, {});
-const eksCluster = new eks.EksCluster(CLUSTER_NAME, {});
+export const gkeCluster = new gke.GkeCluster(CLUSTER_NAME, {});
+export const eksCluster = new eks.EksCluster(CLUSTER_NAME, {});
