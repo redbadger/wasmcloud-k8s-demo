7253a928cc0d727c718e2177d13becbcde9f48cd
attriaayush <aayush.attri@red-badger.com>
2021-08-12 13:47:45 +0100

    updating the delete script to accept cloud provider as arg

--- scripts/delete.sh
+++ scripts/delete.sh
@@ -2,6 +2,19 @@
 set -euxo pipefail
 cd "$(dirname "$0")"/..
 
+CLUSTER="${1:-x}"
+
+if [ "$CLUSTER" == "aws" ]; then
+    aws eks --region eu-west-2 update-kubeconfig --name wasmcloud
+    kubectx aws=. || true
+elif [ "$CLUSTER" == "gcp" ]; then
+    gcloud container clusters get-credentials wasmcloud --zone europe-west2
+    kubectx gcp=. || true
+else
+    echo "Usage: setup.sh gcp | aws"
+    exit 1
+fi
+
 ls -d1 kubernetes/* | sort --reverse | while read dir; do
     kubectl delete -k $dir || echo "$dir failed to delete. Maybe you deleted it already?"
 done
